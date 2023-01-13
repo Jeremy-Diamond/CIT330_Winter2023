db4d3813bd3328bfeb74e61b21af4163ed289822
Jeremy Diamond <socjeremyd@gmail.com>
2023-01-12 19:51:36 -0700

    prettier changes

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
@@ -9,7 +9,15 @@ function addProductToCart(product) {
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  const currentCart = getLocalStorage("so-cart");
+
+  let updatedCart;
+  if (currentCart === null) {
+    updatedCart = product;
+  } else {
+    updatedCart = [product].concat(currentCart);
+  }
+  addProductToCart(updatedCart);
 }
 
 // add listener to Add to Cart button
