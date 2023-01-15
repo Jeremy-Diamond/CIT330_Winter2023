0a4dcb1eb5dfc9365e36a1ccf3b3aec3e66433ff
Jeremy Diamond <socjeremyd@gmail.com>
2023-01-15 02:20:03 -0700

    adjusting for duplicate items

--- src/js/product.js
+++ src/js/product.js
@@ -8,12 +8,34 @@ function addProductToCart(product) {
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
+  const product = {
+    ...(await dataSource.findProductById(e.target.dataset.id)),
+    Qty: 1,
+  };
   const currentCart = getLocalStorage("so-cart");
 
+  //Add Product if cart is empty
   let updatedCart;
   if (currentCart === null) {
     updatedCart = product;
+  } else if (!Array.isArray(currentCart)) {
+    // If cart has one item
+
+    // if product matches update Qty
+    if (product.Id === currentCart.Id) {
+      currentCart.Qty += 1;
+      updatedCart = currentCart;
+    } // if not add line
+    else updatedCart = [product].concat(currentCart);
+  } else if (currentCart.map((item) => item.Id).includes(product.Id)) {
+    // is id in array?
+
+    currentCart[
+      currentCart.findIndex((item) => item.Id === product.Id)
+    ].Qty += 1;
+    updatedCart = currentCart;
+
+    // updatedCart = [product].concat(currentCart);
   } else {
     updatedCart = [product].concat(currentCart);
   }
