1c5ca2c9e023c93e08e6c1122eaa40faff9cf11f
Jeremy <socjeremyd@gmail.com>
2023-01-24 13:29:08 -0700

    Adding Alerts to main page

--- src/js/Alert.js
+++ src/js/Alert.js
@@ -1,47 +1,43 @@
-
 function convertToJson(res) {
-    if (res.ok) {
-      return res.json();
-    } else {
-      throw new Error("Bad Response");
-    }
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Bad Response");
+  }
 }
 
-export default class Alert{
-    constructor(datasource){
-        this.path = `https://raw.githubusercontent.com/Jeremy-Diamond/CIT330_Winter2023/main/src/json/alerts.json`
-        this.alert = {}
-    }
-    
-    async getData() {
-        const res = await fetch(this.path);
+export default class Alert {
+  constructor() {
+  }
+
+   async init(){
+      this.data = await this.getData();
+      if (this.data.length > 0){
+
+        const maindiv = document.querySelector("main"); 
+        let alertsection = document.createElement("section");
+        alertsection.classList.add("alert-list");
+        maindiv.prepend(alertsection);
+        
+        this.data.forEach(alert => {  
+          let alertP = document.createElement("p");
+          alertP.innerHTML = alert.message;
+          alertP.style.backgroundColor = alert.background;
+          alertP.style.color = alert.color;
+          alertsection.appendChild(alertP)
+        });
+
+        //document.prepend(<section class="alert-list"></section>)
+      }
+      }
+      async getData() {
+        const res = await fetch("https://raw.githubusercontent.com/Jeremy-Diamond/CIT330_Winter2023/main/src/json/alerts.json");
         const data = await convertToJson(res);
         return data;
       }
-
-}
-
-
-const newAlert = new Alert("alert") 
-
-console.log(newAlert)  
-
-
-/*
-export default class ProductData {
-    constructor(category) {
-      this.category = category;
-      this.path = `https://raw.githubusercontent.com/Jeremy-Diamond/CIT330_Winter2023/main/src/json/${this.category}.json`;
+      
     }
     
-    async getData() {
-      const res = await fetch(this.path);
-      const data = await convertToJson(res);
-      return data;
-    }
-  
-    async findProductById(id) {
-      const products = await this.getData();
-      return products.find((item) => item.Id === id);
-    }
-*/
+    
+    const newAlert = new Alert()
+    newAlert.init()
