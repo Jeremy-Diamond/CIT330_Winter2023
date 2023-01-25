2dbcb1132dc4ef48978d97ac7fc0f8bdf4ed73ab
Jeremy Diamond <socjeremyd@gmail.com>
2023-01-24 23:27:35 -0700

    Finnishing week 3 strech

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -4,13 +4,13 @@ function productCardTemplate(product) {
     return `<li class="product-card">
     <a href="product_pages/index.html?product=${product.Id}">
     <img
-      src="${product.Image}"
-      alt="Image of ${product.Name}"
+    src="${product.Image}"
+    alt="Image of ${product.Name}"
     />
     <h3 class="card__brand">${product.Brand.Name}</h3>
     <h2 class="card__name">${product.Name}</h2>
     <p class="product-card__price">$${product.FinalPrice}</p></a>
-  </li>`
+    </li>`
 }
 
 export default class ProductList {
@@ -20,11 +20,27 @@ export default class ProductList {
         this.listElement = listElement;
     }
     async init() {
-        const list = await this.dataSource.getData();
+        let list = await this.dataSource.getData();
+        this.filterList(list);
         this.renderList(list);
     }
     renderList(list) {
         renderListWithTemplate(productCardTemplate, this.listElement, list);
     }
+
+    filterList(list){
+
+        list.forEach(item => {
+            if(!["880RR","985RF","985PR","344YJ"].includes(item.Id)){
+                list.splice(
+                    list.findIndex((i)=> item.Id === i.Id),1)
+            }
+        });
+    }
 }
 
+/*
+cartItems.splice(
+    cartItems.findIndex((item) => item.Id === id),
+    1
+  );*/
--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,11 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
-console.log(dataSource);
+//console.log(dataSource);
 
 const listElement = document.querySelector(".product-list");
 
 const productList = new ProductList(dataSource, "tents", listElement);
-console.log(productList);
+//console.log(productList);
 
 productList.init();
