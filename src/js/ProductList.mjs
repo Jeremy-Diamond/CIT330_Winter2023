ed9ed1e9018a054fee8369b11c0922450c7a026a
Jeremy <socjeremyd@gmail.com>
2023-02-10 16:44:43 -0700

    fixing sort

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,7 +1,7 @@
 import { renderListWithTemplate, setProperCase } from "./utils.mjs";
 
 function productCardTemplate(product) {
-    return `<li class="product-card">
+  return `<li class="product-card">
     <a href="../product_pages/index.html?product=${product.Id}">
     <img
     src="${product.Images.PrimaryMedium}"
@@ -10,53 +10,53 @@ function productCardTemplate(product) {
     <h3 class="card__brand">${product.Brand.Name}</h3>
     <h2 class="card__name">${product.Name}</h2>
     <p class="product-card__price">$${product.FinalPrice}</p></a>
-    </li>`
+    </li>`;
 }
 
 export default class ProductList {
-    constructor(dataSource, category, listElement) {
-        this.dataSource = dataSource;
-        this.category = category;
-        this.listElement = listElement;
-    }
-    async init() {
-        const list = await this.dataSource.getData(this.category);
-        document.querySelector("#products-crumb").innerHTML = `${setProperCase(this.category)} (${list.length} Items)`
-        this.renderList(list);
-        document.querySelector(".title").innerHTML = setProperCase(this.category);
-        
+  constructor(dataSource, category, listElement) {
+    this.dataSource = dataSource;
+    this.category = category;
+    this.listElement = listElement;
+  }
+  async init() {
+    const list = await this.dataSource.getData(this.category);
+    document.querySelector("#products-crumb").innerHTML = `${setProperCase(
+      this.category
+    )} (${list.length} Items)`;
 
+    let sortCriteria = "name-asc";
+    const dropdown = document.querySelector("#sort-options");
+    dropdown.addEventListener("change", (event) => {
+      sortCriteria = event.target.value;
+      sortList(sortCriteria, list);
+      this.renderList(list);
+    });
+    this.renderList(list);
+    document.querySelector(".title").innerHTML = setProperCase(this.category);
+  }
 
-        document.querySelector("#sort-options")
-        .addEventListener("change", this.sortList);
-        }
-
-        
-
-      
-    renderList(list) {
-        renderListWithTemplate(productCardTemplate, this.listElement, list);
-    }
-
+  renderList(list) {
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+  }
 }
 
+//console.log(document.querySelector("#sort-options").value);
+//document.querySelector("#sort-options").addEventListener("change",sortList(document.querySelector("#sort-options").value))
 
-
-
-function sortList() {
-    console.log(this.list)
-            switch (this.list) {
-                case "name-asc":
-                this.list.sort((a, b) => (a.Name > b.Name ? 1 : -1));
-                break;
-                case "name-desc":
-                this.list.sort((a, b) => (a.Name < b.Name ? 1 : -1));
-                break;
-                case "price-asc":
-                this.list.sort((a, b) => a.age - b.age);
-                break;
-                case "price-desc":
-                this.list.sort((a, b) => b.age - a.age);
-                break;
-            }
-        };
+function sortList(sortCriteria, list) {
+  switch (sortCriteria) {
+    case "name-asc":
+      list.sort((a, b) => (a.Name > b.Name ? 1 : -1));
+      break;
+    case "name-desc":
+      list.sort((a, b) => (a.Name < b.Name ? 1 : -1));
+      break;
+    case "price-asc":
+      list.sort((a, b) => a.age - b.age);
+      break;
+    case "price-desc":
+      list.sort((a, b) => b.age - a.age);
+      break;
+  }
+}
