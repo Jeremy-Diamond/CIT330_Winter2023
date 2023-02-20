462e58a3ca023ac6ef9778820d92d3dbc6860fdf
WendyFell <90659020+WendyFell@users.noreply.github.com>
2023-02-20 10:32:36 -0700

    add form js and file to store data

---
+++ src/js/form.js
@@ -0,0 +1,22 @@
+// let form = document.forms.my;
+// let elem1 = form.elements.firstName;
+// let elem2 = form.elements.email;
+
+// console.log(elem1.value);
+// console.log(elem2.value);
+
+
+
+// myForm.onsubmit = async (e) => {
+//     e.preventDefault();
+
+//     let response = await fetch("email.txt", {
+//       method: 'POST',
+//       body: new FormData(myForm)
+//     });
+
+//     let result = await response.json();
+
+//     alert(result.message);
+//   };
+
